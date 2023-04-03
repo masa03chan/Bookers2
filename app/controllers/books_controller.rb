bae361e61e51f80b6d2de71c1e9fbd650a2c2ce7
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-04-03 14:56:16 +0000

    [Add]bookindex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,32 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def index
+    @user = current_user
+    @book = Book.new
+
     @books = Book.all
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @user = User.find(@user.id)
+    @book = Book.new
+
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
