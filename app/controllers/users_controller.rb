fdeab6079b41766949e6410933e9ffcada8e0b71
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-04-04 13:58:59 +0000

    [Finish]user_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,31 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    
+
     @book = Book.new
   end
 
   def index
     @user = current_user
     @users = User.all
-    
+
     @book = Book.new
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 end
