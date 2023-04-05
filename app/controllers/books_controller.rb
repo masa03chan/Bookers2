3777d4403594167bf842dc7ff05de0133cefcd7a
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-04-05 14:49:00 +0000

    [Add]Validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,32 +10,41 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
-    
+
     @books = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   private
